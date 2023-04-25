76ac39ec121062c3f34b74757d6ff11f2170e1fa
Henrique Andrade <henrique.ccda@gmail.com>
2023-04-25 17:09:38 -0300

    m

--- curso3/tracker/src/store/index.ts
+++ curso3/tracker/src/store/index.ts
@@ -1,9 +1,9 @@
 import IProjeto from "@/interfaces/IProjeto";
 import { createStore, Store, useStore as vuexUseStore } from "vuex";
 import { InjectionKey } from 'vue'
-import { ADICIONA_PROJETO, ALTERA_PROJETO, DEFINIR_PROJETOS, DEFINIR_TAREFAS, EXCLUIR_PROJETO, NOTIFICAR } from "./tipo-mutacoes";
+import { ADICIONA_PROJETO, ADICIONA_TAREFA, ALTERA_PROJETO, ALTERA_TAREFA, DEFINIR_PROJETOS, DEFINIR_TAREFAS, EXCLUIR_PROJETO, NOTIFICAR } from "./tipo-mutacoes";
 import { INotificacao } from "@/interfaces/INotificacao";
-import { ALTERAR_PROJETO, CADASTRAR_PROJETO, OBTER_PROJETOS, OBTER_TAREFAS, REMOVER_PROJETO } from "./tipo-acoes";
+import { ALTERAR_PROJETO, ALTERAR_TAREFA, CADASTRAR_PROJETO, CADASTRAR_TAREFA, OBTER_PROJETOS, OBTER_TAREFAS, REMOVER_PROJETO } from "./tipo-acoes";
 import http from "@/http"
 import ITarefa from "@/interfaces/ITarefa";
 
@@ -42,6 +42,13 @@ export const store = createStore<Estado>({
         [DEFINIR_TAREFAS](state, tarefas: ITarefa[]) {
             state.tarefas = tarefas
         },
+        [ADICIONA_TAREFA](state, tarefa: ITarefa) {
+            state.tarefas.push(tarefa)
+        },
+        [ALTERA_TAREFA](state, tareda: ITarefa) {
+            const index = state.tarefas.findIndex(t => t.id == tareda.id)
+            state.tarefas[index] = tareda
+        },
         [NOTIFICAR] (state, novaNotificacao: INotificacao) {
 
             novaNotificacao.id = new Date().getTime()
@@ -72,6 +79,14 @@ export const store = createStore<Estado>({
         [OBTER_TAREFAS] ({ commit }) {
             http.get('/tarefas')
                 .then(resposta => commit(DEFINIR_TAREFAS, resposta.data))
+        },
+        [CADASTRAR_TAREFA] ({commit}, tarefa: ITarefa) {
+            return http.post('/tarefas', tarefa)
+                .then(resposta => commit(ADICIONA_TAREFA, resposta.data))
+        },
+        [ALTERAR_TAREFA] ({ commit }, tarefa: ITarefa) {
+            return http.put(`/tarefas/${tarefa.id}`, tarefa)
+                .then(() => commit(ALTERA_TAREFA, tarefa))
         },
     }
 })
--- curso3/tracker/src/views/Tarefas.vue
+++ curso3/tracker/src/views/Tarefas.vue
@@ -1,12 +1,42 @@
 <template>
-    <!-- <Formulario @aoSalvarTarefa="salvarTarefa"/> -->
-    <Formulario />
-    <div class="lista">
-        <Box v-if="semTarefas">
-            Você não está muito produtivo hoje <span class="has-text-weight-bold">:(</span>
-        </Box>
-        <Tarefa v-for="(tarefa, index) in tarefas" :tarefa="tarefa" :key="index"/>
+  <Formulario @aoSalvarTarefa="salvarTarefa" />
+  <div class="lista">
+    <Box v-if="semTarefas">
+      Você não está muito produtivo hoje <span class="has-text-weight-bold">:(</span>
+    </Box>
+    <Tarefa
+      v-for="(tarefa, index) in tarefas"
+      :tarefa="tarefa"
+      :key="index"
+      @aoTarefaClicada="selecionarTarefa"
+    />
+    <div class="modal" :class="{ 'is-active': tarefaSelecionada }" v-if="tarefaSelecionada">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Editando uma tarefa</p>
+          <button class="delete" aria-label="close"></button>
+        </header>
+        <section class="modal-card-body">
+          <div class="field">
+            <label for="DescricaoDaTarefa" class="label">
+              Descricao
+            </label>
+            <input
+              type="text"
+              class="input"
+              v-model="tarefaSelecionada.descricao"
+              id="DescricaoDaTarefa"
+            />
+          </div>
+        </section>
+        <footer class="modal-card-foot">
+          <button @click="alterarTarefa" class="button is-success">Salvar alterações</button>
+          <button @click="fecharModal" class="button">Cancelar</button>
+        </footer>
+      </div>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -15,7 +45,8 @@ import Formulario from "../components/Formulario.vue";
 import Tarefa from "../components/Tarefa.vue";
 import Box from "../components/Box.vue";
 import { useStore } from "@/store";
-import { OBTER_TAREFAS } from "@/store/tipo-acoes";
+import { CADASTRAR_TAREFA, OBTER_PROJETOS, OBTER_TAREFAS, ALTERAR_TAREFA } from "@/store/tipo-acoes";
+import ITarefa from "@/interfaces/ITarefa";
 
 export default defineComponent({
   name: "App",
@@ -24,21 +55,37 @@ export default defineComponent({
     Tarefa,
     Box
   },
+  data() {
+    return {
+      tarefaSelecionada: null as ITarefa | null
+    }
+  },
   methods: {
-    // salvarTarefa (tarefa:ITarefa) : void {
-    //   this.tarefas.push(tarefa)
-    // }
+    salvarTarefa(tarefa: ITarefa): void {
+      this.store.dispatch(CADASTRAR_TAREFA, tarefa)
+    },
+    selecionarTarefa(tarefa: ITarefa){
+      this.tarefaSelecionada = tarefa
+    },
+    fecharModal () {
+      this.tarefaSelecionada = null
+    },
+    alterarTarefa () {
+      this.store.dispatch(ALTERAR_TAREFA, this.tarefaSelecionada)
+        .then(() => this.fecharModal())
+    }
   },
   computed: {
-    semTarefas () :boolean {
+    semTarefas(): boolean {
       return this.tarefas.length == 0
     }
   },
   setup() {
     const store = useStore()
+    store.dispatch(OBTER_PROJETOS)
     store.dispatch(OBTER_TAREFAS)
     return {
-      tarefas: computed(()=> store.state.tarefas),
+      tarefas: computed(() => store.state.tarefas),
       store
     }
   }
